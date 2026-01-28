f28f21aa2a24d1727695033b68d402a33e3b4653
Nikki1504 <thakurnikita157@gmail.com>
2026-01-28 23:44:56 +0530

    move firebase config to environment variables

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -8,12 +8,12 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDtXe15VkKi0KSOzWZyqVm9oUYaVF6lMfk",
-  authDomain: "netflixgpt-99ad6.firebaseapp.com",
-  projectId: "netflixgpt-99ad6",
-  storageBucket: "netflixgpt-99ad6.firebasestorage.app",
-  messagingSenderId: "33247829106",
-  appId: "1:33247829106:web:21f8336e638f3078bd36eb",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: "G-6BFNW46ZC8",
 };
 
